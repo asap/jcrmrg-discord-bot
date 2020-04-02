47049389a01ebd953bbf6de2c8821f264988f570
ASAP <alexander.sapountzis@gmail.com>
2020-04-02 14:29:01 -0400

    Add Knowledge of guilds

--- bot.py
+++ bot.py
@@ -5,11 +5,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} is a great person')
+    for guild in client.guilds:
+            if guild.name == GUILD:
+                break
+
+    print(f'{client.user} is in the following guild(s):\n')
+    print(f'{guild.name} - {guild.id}')
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
